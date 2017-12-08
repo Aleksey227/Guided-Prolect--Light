5df03e1c08b7c95e88df77bea97b941e8cff55b5
student <student@tsn-elector.ttknn.net>
2017-12-08 15:02:18 +0300

    uerhn jgndfrngdfk

--- Guided Prolect--Light/ViewController.swift
+++ Guided Prolect--Light/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isLightOn = true
+    
+    @IBAction func buy(_ sender: Any) {
+        isLightOn = !isLightOn
+        if isLightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .brown
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
